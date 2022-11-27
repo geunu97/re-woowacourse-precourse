4037e46c681751341c2d405d2253a9dd5b53bf59
olo90632951 <olo90632951@gmail.com>
2022-11-27 16:50:39 +0900

    feat: problem3 풀이 추가

--- javascript-onboarding/src/problem3.js
+++ javascript-onboarding/src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  return getCount369(number);
 }
+
+const getCount369 = (number) => {
+  let count = 0;
+  for (let currentNumber = 1; currentNumber <= number; currentNumber += 1) {
+    const matchNumber = String(currentNumber).match(/3|6|9/g);
+    if (matchNumber) {
+      count += matchNumber.length;
+    }
+  }
+  return count;
+};
 
 module.exports = problem3;
