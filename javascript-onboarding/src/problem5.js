540fcf3a020bc2aa23c76656f5997bfd7a4a6031
olo90632951 <olo90632951@gmail.com>
2022-11-27 17:52:05 +0900

    feat: problem5 풀이 추가

--- javascript-onboarding/src/problem5.js
+++ javascript-onboarding/src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  return convertMoney(money, [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]);
 }
+
+const convertMoney = (remainingMoney, moneyArray) => {
+  return moneyArray.map((money) => {
+    const dividedMoney = Math.floor(remainingMoney / money);
+    remainingMoney = remainingMoney % money;
+    return dividedMoney;
+  });
+};
 
 module.exports = problem5;
