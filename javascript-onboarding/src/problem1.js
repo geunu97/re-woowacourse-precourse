c394e601715e4547e76cee49d1d196acdb39ba6f
olo90632951 <olo90632951@gmail.com>
2022-11-27 15:22:38 +0900

    feat: problem1 풀이 추가

--- javascript-onboarding/src/problem1.js
+++ javascript-onboarding/src/problem1.js
@@ -1,6 +1,61 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (isValidatePage(pobi) && isValidatePage(crong)) {
+    return getGameResult(getLargestNumber(pobi), getLargestNumber(crong));
+  }
+  return -1;
 }
+
+const getGameResult = (pobiLargestNumber, crongLargestNumber) => {
+  if (pobiLargestNumber === crongLargestNumber) {
+    return 0;
+  }
+  if (pobiLargestNumber > crongLargestNumber) {
+    return 1;
+  }
+  if (pobiLargestNumber < crongLargestNumber) {
+    return 2;
+  }
+};
+
+const getLargestNumber = (pages) => {
+  const result = [];
+  pages.forEach((page) => {
+    result.push(getSumPage(page), getMulPage(page));
+  });
+
+  return Math.max(...result);
+};
+
+const getSumPage = (page) => {
+  return String(page)
+    .split("")
+    .reduce((acc, currentValue) => {
+      return acc + Number(currentValue);
+    }, 0);
+};
+
+const getMulPage = (page) => {
+  return String(page)
+    .split("")
+    .reduce((acc, currentValue) => {
+      return acc * Number(currentValue);
+    }, 1);
+};
+
+const isValidatePage = ([leftPage, rightPage]) => {
+  if (!Number(leftPage) || !Number(rightPage)) {
+    return false;
+  }
+  if (Number(leftPage) % 2 !== 1 || Number(rightPage) % 2 !== 0) {
+    return false;
+  }
+  if (Number(leftPage) + 1 !== Number(rightPage)) {
+    return false;
+  }
+  if (Number(leftPage) < 1 || Number(rightPage) > 400) {
+    return false;
+  }
+  return true;
+};
 
 module.exports = problem1;
