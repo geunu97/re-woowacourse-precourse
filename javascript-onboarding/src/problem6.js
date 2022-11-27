581071fd374fb6bb22361bb25aad4034ae2c750b
olo90632951 <olo90632951@gmail.com>
2022-11-27 19:04:03 +0900

    feat: problem6  풀이 추가

--- javascript-onboarding/src/problem6.js
+++ javascript-onboarding/src/problem6.js
@@ -1,6 +1,41 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const duplicatedWordUserEmails = getDuplicatedWordUserEmails(
+    setDividedWords(forms, new Map())
+  );
+  return sortArray(duplicatedWordUserEmails);
 }
+
+const sortArray = (array) => {
+  return array.sort();
+};
+
+const removeDuplicatedValue = (array) => {
+  return Array.from(new Set(array));
+};
+
+const getDuplicatedWordUserEmails = (dividedWords) => {
+  const userEmails = [];
+  dividedWords.forEach((emails) => {
+    if (emails.length >= 2) {
+      userEmails.push(...emails);
+    }
+  });
+  return removeDuplicatedValue(userEmails);
+};
+
+const setDividedWords = (forms, dividedWords) => {
+  forms.forEach(([email, nickName]) => {
+    for (let index = 0; index <= nickName.length - 2; index += 1) {
+      const dividedNickName = nickName.slice(index, index + 2);
+      if (dividedWords.has(dividedNickName)) {
+        const setEmails = [...dividedWords.get(dividedNickName), email];
+        dividedWords.set(dividedNickName, removeDuplicatedValue(setEmails));
+        continue;
+      }
+      dividedWords.set(dividedNickName, [email]);
+    }
+  });
+  return dividedWords;
+};
 
 module.exports = problem6;
