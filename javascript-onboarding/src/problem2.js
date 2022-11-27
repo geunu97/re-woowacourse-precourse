50c24146f77a4158826259c68c61c6d00301bde3
olo90632951 <olo90632951@gmail.com>
2022-11-27 15:36:10 +0900

    feat: problem2 풀이 추가

--- javascript-onboarding/src/problem2.js
+++ javascript-onboarding/src/problem2.js
@@ -1,6 +1,21 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  return removeDuplicatedWord(cryptogram);
 }
+
+const removeDuplicatedWord = (cryptogram) => {
+  const stack = [];
+  cryptogram.split("").forEach((word) => {
+    if (stack.length === 0) {
+      stack.push(word);
+      return;
+    }
+    if (stack[stack.length - 1] === word) {
+      stack.pop();
+      return;
+    }
+    stack.push(word);
+  });
+  return stack.join("");
+};
 
 module.exports = problem2;
