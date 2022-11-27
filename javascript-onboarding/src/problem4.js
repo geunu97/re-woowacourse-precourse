5c34cc6fe7780543edce7d7b86868668bb10709e
olo90632951 <olo90632951@gmail.com>
2022-11-27 17:28:49 +0900

    feat: problem4 풀이 추가

--- javascript-onboarding/src/problem4.js
+++ javascript-onboarding/src/problem4.js
@@ -1,6 +1,17 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  return replaceWord(word).join("");
 }
+
+const replaceWord = (word) => {
+  return word.split("").map((value) => {
+    if ("A" <= value && value <= "Z") {
+      return String.fromCharCode(90 - (value.charCodeAt(0) - 65));
+    }
+    if ("a" <= value && value <= "z") {
+      return String.fromCharCode(122 - (value.charCodeAt(0) - 97));
+    }
+    return value;
+  });
+};
 
 module.exports = problem4;
