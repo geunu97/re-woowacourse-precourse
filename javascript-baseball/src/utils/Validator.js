81b4fe9e0326966f2c74be9f87a7b73cc1b5c8c8
olo90632951 <olo90632951@gmail.com>
2022-11-28 18:17:30 +0900

    feat: 입력 받는 사용자 수 예외 발생 기능 추가

---
+++ javascript-baseball/src/utils/Validator.js
@@ -0,0 +1,19 @@
+const Validator = {
+  isUserNumbers(numbers) {
+    if (!Number(numbers)) {
+      return false;
+    }
+    if (numbers.includes('0')) {
+      return false;
+    }
+    if (new Set(numbers).size !== numbers.length) {
+      return false;
+    }
+    if (numbers.length !== 3) {
+      return false;
+    }
+    return true;
+  },
+};
+
+module.exports = Validator;
---
+++ javascript-baseball/src/view/InputView.js
@@ -0,0 +1,15 @@
+const { Console } = require('@woowacourse/mission-utils');
+const Validator = require('../utils/Validator');
+
+const InputView = {
+  readUserNumbers(callback) {
+    Console.readLine('숫자를 입력해주세요 : ', (numbers) => {
+      if (!Validator.isUserNumbers(numbers)) {
+        throw new Error('[ERROR] 1부터 9까지 서로 다른 3자리 수로 이루어져야 합니다.');
+      }
+      callback(number);
+    });
+  },
+};
+
+module.exports = InputView;
