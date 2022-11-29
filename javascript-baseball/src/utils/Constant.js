93c2f6c7aaa55edcbd52c5c771a4470bdd498477
olo90632951 <olo90632951@gmail.com>
2022-11-29 19:37:42 +0900

    refactor: constant 상수화

--- javascript-baseball/src/utils/Constant.js
+++ javascript-baseball/src/utils/Constant.js
@@ -12,7 +12,7 @@ const COMMAND = Object.freeze({
 
 const INPUT_MESSAGE = Object.freeze({
   USER_NUMBERS: '숫자를 입력해주세요 : ',
-  FINAL_COMMAND: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+  FINAL_COMMAND: `게임을 새로 시작하려면 ${COMMAND.REPLAY}, 종료하려면 ${COMMAND.END}를 입력하세요.\n`,
 });
 
 const OUTPUT_MESSAGE = Object.freeze({
@@ -21,7 +21,7 @@ const OUTPUT_MESSAGE = Object.freeze({
   BALL: (ball) => `${ball}볼`,
   STRIKE: (strike) => `${strike}스트라이크`,
   NOTHING: '낫싱',
-  COMPLETE: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  COMPLETE: `${GAME_RULE.COMPLETE_STRIKE_COUNT}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
 });
 
 const ERROR_MESSAGE_HEADER = '[ERROR]';
@@ -30,11 +30,11 @@ const ERROR_MESSAGE_USER_NUMBERS = Object.freeze({
   TYPE: `${ERROR_MESSAGE_HEADER} 숫자가 아닙니다.`,
   INCLUDE_ZERO: `${ERROR_MESSAGE_HEADER} 0이 포함되어 있으면 안됩니다.`,
   DUPLICATE: `${ERROR_MESSAGE_HEADER} 중복된 수가 존재하면 안됩니다.`,
-  LENGTH: `${ERROR_MESSAGE_HEADER} 길이가 3이 아닙니다.`,
+  LENGTH: `${ERROR_MESSAGE_HEADER} 길이가 ${GAME_RULE.LENGTH}이 아닙니다.`,
 });
 
 const ERROR_MESSAGE_FINAL_COMMAND = Object.freeze({
-  COMMAND: `${ERROR_MESSAGE_HEADER} 1(재시작) 또는 2(종료)를 입력해야 합니다.`,
+  COMMAND: `${ERROR_MESSAGE_HEADER} ${COMMAND.REPLAY}(재시작) 또는 ${COMMAND.END}(종료)를 입력해야 합니다.`,
 });
 
 module.exports = {
