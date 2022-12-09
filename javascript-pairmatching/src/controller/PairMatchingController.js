1d1751deeb553fa44d7739db81e2369b084a63f2
olo90632951 <olo90632951@gmail.com>
2022-12-09 17:31:34 +0900

    feat: 페어 매칭의 과정과 미션을 출력하는 기능 추가

--- javascript-pairmatching/src/controller/PairMatchingController.js
+++ javascript-pairmatching/src/controller/PairMatchingController.js
@@ -1,9 +1,13 @@
 const InputView = require('../view/InputView');
+const OutputView = require('../view/OutputView');
 
 class PairMatchingController {
   start() {
     InputView.readFunctionCommand((command) => {
-      console.log(command);
+      if (command === '1') {
+        OutputView.printPairMatching();
+        return;
+      }
     });
   }
 }
